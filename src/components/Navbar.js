b7d6a63c21692ed3c1b99397b7ab356abb491a83
bawumbila <74751215+bawumbila@users.noreply.github.com>
2024-08-14 17:33:17 -0400

    added more components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { Button } from "./Button";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            TRVL <i className="fa-brands fa-typo3" />
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            XPDITION &nbsp;
+            <i className="fa-solid fa-earth-americas" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fa-solid fa-times" : "fa-solid fa-bars"} />
@@ -39,7 +58,17 @@ function Navbar() {
                 Products
               </Link>
             </li>
+            <li className="=nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
           </ul>
+          {button && <Button>SIGN UP</Button>}
         </div>
       </nav>
     </>
